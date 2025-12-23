76b3916bec35c4bf4f0eea9efb097cd12bd97b0e
MaithiliBhurke <maithilibhurke10.model@gmail.com>
2025-12-23 23:06:59 +0530

    server created and mongodb is connected

---
+++ Backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+const app =express();
+
+app.use(express.json())
+
+// require('dotenv').config();
+// const PORT=5050;
+
+const PORT=process.env.PORT||5050;
+const MONGO_URL=process.env.MONGO_URL;
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB error:", err));
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello')
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening at ${PORT}`)
+});
+
+
+
